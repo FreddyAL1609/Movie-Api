d1be920ef4b07e1a70c5c05aaa72482d09a15e3d
Freddy  Cruz Chinguel <FRED_16_09@hotmail.com>
2023-03-06 12:30:32 -0500

    Deploy

---
+++ movie/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# id son autogenerados
+class Movie(models.Model):
+   
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    year=models.IntegerField()
+    rating=models.FloatField()
+    genre=models.CharField(max_length=50)
+    image=models.TextField()
+    country=models.CharField(max_length=50)
+    duration=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    
+    class Meta:
+        db_table="movies"
+        
+    
