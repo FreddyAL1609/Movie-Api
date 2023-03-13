9ca11be451cde37c52636ee08e4d33077a41591b
Freddy  Cruz Chinguel <FRED_16_09@hotmail.com>
2023-03-12 20:54:16 -0500

    Railway11

--- movie/views.py
+++ movie/views.py
@@ -7,4 +7,4 @@ from rest_framework.permissions import IsAuthenticated
 class MovieViewSet(viewsets.ModelViewSet):
     queryset=Movie.objects.all()
     serializer_class=MovieSerializer
-    permission_classes=[IsAuthenticated]
+    #permission_classes=[IsAuthenticated]
